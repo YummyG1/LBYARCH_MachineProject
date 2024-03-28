f6d8a00eb89c8869b5e452b1262f1dfdc52e52b6
Yummy <ggmuriel321@gmail.com>
2024-03-28 16:24:40 +0800

    fix minor spacing of outputs and freed memory allocation for X,Y and Z

--- Project1/Muriel_Villamiel.c
+++ Project1/Muriel_Villamiel.c
@@ -51,10 +51,16 @@ int main() {
 	clock_t end = clock();
 	double cpu_time_used = ((double)(end - start)) / CLOCKS_PER_SEC;
 
-	printf("Result:\n");
+	printf("\nResult:\n");
 	for (int i = 0; i < 10; ++i) {
 		printf("%.2f ", Z[i]);
 	}
 	printf("\nExecution time: %f seconds\n", cpu_time_used);
+
+	// Free the dynamically allocated memory when done
+	free(X);
+	free(Y);
+	free(Z);
+
 	return 0;
 }
