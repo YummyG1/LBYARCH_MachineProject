da3e2c531a38fb12b50e6fd6624765107125e386
Yummy <ggmuriel321@gmail.com>
2024-03-28 16:15:16 +0800

    C is working, Sanity check until 2^30 with display of execution time

--- Project1/Muriel_Villamiel.c
+++ Project1/Muriel_Villamiel.c
@@ -4,48 +4,54 @@
 #include <stdint.h>
 #include <time.h>
 
-#define N 1073741823
-
-extern void asmhello();
-
-void saxpy_c(float A, float X[], float Y[], float Z[], int n) {
-	for (int i = 0; i < n; ++i) {
-		Z[i] = A * X[i] + Y[i];
-	}
-}
 
 
 int main() {
-	asmhello();
 
 	int nExp;
+	int nVal = 1;
+	float A = 2.0f;
+	float *X, *Y, *Z;
+
 	printf("Enter the exponent of 2: ");
 	if (scanf_s("%d", &nExp) != 1) {
 		printf("Invalid input. Please enter an integer.\n");
 		return 1; // Exiting due to invalid input
 	}
-	int nVal = 1;
+
 	for (int j = 0; j < nExp; j++) {
 		 nVal*= 2;
 	}
 
-	printf("value %d", nVal);
+	printf("value %d \n", nVal);
 
-	float A = 2.0f;
-	float X[N], Y[N], Z[N];
+	X = (float*)malloc(nVal * sizeof(float));
+	Y = (float*)malloc(nVal * sizeof(float));
+	Z = (float*)malloc(nVal * sizeof(float));
 
 	for (int i = 0; i < nVal; ++i) {
-		X[i] = (float)rand() / RAND_MAX;
-		Y[i] = (float)rand() / RAND_MAX;
+		if (i == 0) {
+			X[i] = 1.0f;
+			Y[i] = 11.0f;
+		}
+
+		if (i > 0) {
+			X[i] = 1.0f + X[i - 1];
+			Y[i] = 1.0f + Y[i - 1];
+		}
 	}
 
-	saxpy_c(A, X, Y, Z, nVal);
+	clock_t start = clock();
+	for (int i = 0; i < nVal; ++i) {
+		Z[i] = (A * X[i]) + Y[i];
+	}
+	clock_t end = clock();
+	double cpu_time_used = ((double)(end - start)) / CLOCKS_PER_SEC;
 
 	printf("Result:\n");
 	for (int i = 0; i < 10; ++i) {
 		printf("%.2f ", Z[i]);
 	}
-	printf("\n");
-
+	printf("\nExecution time: %f seconds\n", cpu_time_used);
 	return 0;
 }
