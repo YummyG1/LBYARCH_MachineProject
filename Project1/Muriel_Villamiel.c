a9bb9695978dff849f345e67bb4d8dabe651dc3c
Leeroi Villamiel <denisvillamiel@gmail.com>
2024-04-02 19:06:26 +0800

    Added for loop for 30 runs per kernel

--- Project1/Muriel_Villamiel.c
+++ Project1/Muriel_Villamiel.c
@@ -47,37 +47,57 @@ int main() {
 			Y[i] = 1.0f + Y[i - 1];
 		}
 	}
+	double cpu_time_usedC = 0;
+	double cpu_time_usedC_overall = 0;
+	double cpu_time_usedASM = 0;
+	double cpu_time_usedASM_overall = 0;
 
-	clock_t start = clock();
-	saxpy_c(A, X, Y, Z, nVal);
-	clock_t end = clock();
-	double cpu_time_usedC = ((double)(end - start)) / CLOCKS_PER_SEC;
+	for (int i = 0; i < 30; i++)
+	{
+		clock_t start = clock();
+			saxpy_c(A, X, Y, Z, nVal);
+		clock_t end = clock();
+
+		cpu_time_usedC = ((double)(end - start)) / CLOCKS_PER_SEC;
+		cpu_time_usedC_overall += cpu_time_usedC;
+
+		free(X);
+		free(Y);
+		free(Z);
+	}
 
 	printf("\nFirst 10 results in C:\n");
-	for (int i = 0; i < 10; ++i) {
+	for (int i = 0; i < 10; ++i) 
+	{
 		printf("%.2f ", Z[i]);
 	}
-	printf("\nExecution time in C: %f seconds\n", cpu_time_usedC);
+	printf("\nOverall execution time in C with a vector size of 2^%d: %f seconds\n", nExp, cpu_time_usedC);
 
-	start = clock();
-	for (int i = 0; i < nVal; ++i) {
-		Z[i] = asmfunc(A, X[i], Y[i]);
+	for (int i = 0; i < 30; i++) 
+	{
+		clock_t start = clock();
+		for (int i = 0; i < nVal; ++i) {
+			Z[i] = asmfunc(A, X[i], Y[i]);
+		}
+		clock_t end = clock();
+		cpu_time_usedASM = ((double)(end - start)) / CLOCKS_PER_SEC;
+		cpu_time_usedASM_overall += cpu_time_usedASM;
+
+		free(X);
+		free(Y);
+		free(Z);
 	}
-	end = clock();
-	double cpu_time_usedASM = ((double)(end - start)) / CLOCKS_PER_SEC;
 
 	printf("\nFirst 10 results in ASM:\n");
 	for (int i = 0; i < 10; ++i) {
 		printf("%.2f ", Z[i]);
 	}
-	printf("\nExecution time in ASM: %f seconds\n", cpu_time_usedASM);
+	printf("\nOverall execution time in ASM with a vector size of 2^%d: %f seconds\n", nExp, cpu_time_usedASM);
 
 
 
 	// Free the dynamically allocated memory when done
-	free(X);
-	free(Y);
-	free(Z);
+
 
 	return 0;
 }
